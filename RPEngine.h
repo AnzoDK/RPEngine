a7c02f1ffc256c56507b2fa8432ca4ddff917e80
Anzo <antonr@live.dk>
2020-08-01 15:35:38 +0200

    Update RPEngine.h

--- RPEngine.h
+++ RPEngine.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "../includes/RPAudio.h"
+#ifdef __CPP17__
+#include <filesystem>
+#endif
 #ifdef _WIN32
 #include <windows.h>
 #endif
@@ -12,6 +15,8 @@ struct CharacterSprite
 {
   public:
     std::string path;
+  private:
+    uint8_t* buffer;
     
 }
 
@@ -20,12 +25,16 @@ enum CharacterState{Default=0,Smiling,Crying,Annoyed,Sad};
 class CharacterObject
 {
   public:
-    ChangeState(CharacterState state);
+    //Takes a folder and reads all files in the folder as sprites - as of now it uses the CharacterState as namingscheme. so 0.png would be default for the character
+    void BuildCharacterObject(std::string characterFolder);
+    CharacterObject();
+    void ChangeState(CharacterState state);
+    CharacterState GetState();
     ~CharacterObject();
   private:
     int currState;
     //Holds the characters sprites for different states - Should always be ordered in the same way as CharacterState (i.e Default sprite would be in slot 0 and so on)
-    std::vector<CharacterSprite> chrStates;
+    std::vector<CharacterSprite*> chrStates;
 }
 
 class Scene
@@ -39,7 +48,7 @@ class RosenoernEngine
   public:
   RosenoernEngine();
   ~RosenoernEngine();
-  init();
+  void init();
   private:
     RosenoernAudio* audio;
 };
