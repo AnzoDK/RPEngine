9c23f5af37e445f9c47bd18a63c6102637e0a9dd
AnzoDK <antonr@live.dk>
2020-09-01 12:57:32 +0200

    Changed default rendercolor to white instead of red

--- includes/RPUI.h
+++ includes/RPUI.h
@@ -12,7 +12,7 @@ namespace rp
     
     struct TextureModulator
     {
-        TextureModulator(){modR = 0; modG = 0; modB = 0; modA = 255;}
+        TextureModulator(){modR = 1; modG = 1; modB = 1; modA = 255;}
         Uint8 modR;
         Uint8 modG;
         Uint8 modB;
