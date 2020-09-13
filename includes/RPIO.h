b2e4df88f0e46d1e5a00134a86f8273137b0c749
AnzoDK <antonr@live.dk>
2020-09-13 16:26:21 +0200

    I know I said, that I was too tired to find out wtf was wrong, but I just realized that buffer isn't always initilized. That should be fixed now

--- includes/RPIO.h
+++ includes/RPIO.h
@@ -13,7 +13,7 @@ namespace rp
 
  struct RawFile
  {
-     RawFile(){}
+     RawFile(){length = 0; path = ""; buffer = new char[5]{'E','M','P','Y'};}
      RawFile(char* buff, int _length,std::string _path){/*buffer = new char[_length];for(int i = 0; i < _length; i++){buffer[i] = buff[i];}*/buffer = buff; length = _length; path = _path;}
      ~RawFile(){delete[] buffer;} //<--- This fucks it up
      char* buffer;
