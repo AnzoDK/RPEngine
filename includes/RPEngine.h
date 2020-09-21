6cf16e3f636ffc9107ba93e419d2f3e10868aa40
AnzoDK <antonr@live.dk>
2020-09-21 21:49:50 +0200

    comment update

--- includes/RPEngine.h
+++ includes/RPEngine.h
@@ -1,5 +1,5 @@
 /*
-Coded by AnzoDK (https://github.com/AnzoDK) for Rosenørn Productions (rosenoern-productions.dk) Please refer to the LICENCE file for more info about copying and sharing
+Coded by AnzoDK (https://github.com/AnzoDK) for Rosenørn Productions (rosenoern-productions.dk) Please refer to the LICENCE.md file for more info about copying and sharing
 */
 #pragma once
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
