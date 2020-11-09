de8902ae7772334df56dc148628a7fb14751e6a1
Anzo <antonr@live.dk>
2020-11-09 17:35:41 +0100

    Update buildcheck.sh

--- buildcheck.sh
+++ buildcheck.sh
@@ -5,7 +5,7 @@ g++ check.cpp -o test.o >> /dev/null
 if [ $? -ne 0 ]
 then
   echo "RPAudio is not correctly installed - building against a local copy instead!"
-  $LOCAL=1
+  LOCAL=1
 else
   rm test.o
 fi
